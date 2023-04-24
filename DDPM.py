7cce7def19bf71f3f1a97e6f6418d017c92b4e30
ttdow <travis.t.dow@gmail.com>
2023-04-24 17:55:10 -0600

    Working diffusion image generator. Started NeRF implementation.

--- DDPM.py
+++ DDPM.py
@@ -4,12 +4,158 @@ import torch
 import torchvision
 from PIL import Image
 import matplotlib.pyplot as plt
+import einops
+import imageio
+from tqdm.auto import tqdm
 
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.optim import Adam
 import torchvision.transforms as T
 from torchvision.transforms import Compose, ToTensor, Lambda
 from torchvision.datasets.mnist import MNIST, FashionMNIST
+
+def sinusoidal_embedding(n, d):
+
+    # Returns the standard positional embedding
+    embedding = torch.zeros(n, d)
+    wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
+    wk = wk.reshape((1, d))
+    t = torch.arange(n).reshape((n,1))
+    embedding[:,::2] = torch.sin(t * wk[:,::2])
+    embedding[:,1::2] = torch.cos(t * wk[:,::2])
+
+    return embedding
+
+class MyBlock(nn.Module):
+    def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+        super(MyBlock, self).__init__()
+
+        self.ln = nn.LayerNorm(shape)
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
+        self.activation = nn.SiLU() if activation is None else activation
+        self.normalize = normalize
+
+    def forward(self, x):
+        out = self.ln(x) if self.normalize else x
+        out = self.conv1(out)
+        out = self.activation(out)
+        out = self.conv2(out)
+        out = self.activation(out)
+
+        return out
+
+class UNet(nn.Module):
+    def __init__(self, n_steps=1000, time_emb_dim=100):
+        super(UNet, self).__init__()
+
+        # Sinusoidal embedding
+        self.time_embed = nn.Embedding(n_steps, time_emb_dim)
+        self.time_embed.weight.data = sinusoidal_embedding(n_steps, time_emb_dim)
+        self.time_embed.requires_grad_(False)
+
+        # First half
+        self.te1 = self._make_te(time_emb_dim, 1)
+        self.b1 = nn.Sequential(
+            MyBlock((1, 28, 28), 1, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10)
+        )
+        self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
+
+        self.te2 = self._make_te(time_emb_dim, 10)
+        self.b2 = nn.Sequential(
+            MyBlock((10, 14, 14), 10, 20),
+            MyBlock((20, 14, 14), 20, 20),
+            MyBlock((20, 14, 14), 20, 20)
+        )
+        self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
+
+        self.te3 = self._make_te(time_emb_dim, 20)
+        self.b3 = nn.Sequential(
+            MyBlock((20, 7, 7), 20, 40),
+            MyBlock((40, 7, 7), 40, 40),
+            MyBlock((40, 7, 7), 40, 40)
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(40, 40, 2, 1),
+            nn.SiLU(),
+            nn.Conv2d(40, 40, 4, 2, 1)
+        )
+
+        # Bottleneck
+        self.te_mid = self._make_te(time_emb_dim, 40)
+        self.b_mid = nn.Sequential(
+            MyBlock((40, 3, 3), 40, 20),
+            MyBlock((20, 3, 3), 20, 20),
+            MyBlock((20, 3, 3), 20, 40)
+        )
+
+        # Second half
+        self.up1 = nn.Sequential(
+            nn.ConvTranspose2d(40, 40, 4, 2, 1),
+            nn.SiLU(),
+            nn.ConvTranspose2d(40, 40, 2, 1)
+        )
+
+        self.te4 = self._make_te(time_emb_dim, 80)
+        self.b4 = nn.Sequential(
+            MyBlock((80, 7, 7), 80, 40),
+            MyBlock((40, 7, 7), 40, 20),
+            MyBlock((20, 7, 7), 20, 20)
+        )
+
+        self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
+        self.te5 = self._make_te(time_emb_dim, 40)
+        self.b5 = nn.Sequential(
+            MyBlock((40, 14, 14), 40, 20),
+            MyBlock((20, 14, 14), 20, 10),
+            MyBlock((10, 14, 14), 10, 10)
+        )
+
+        self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
+        self.te_out = self._make_te(time_emb_dim, 20)
+        self.b_out = nn.Sequential(
+            MyBlock((20, 28, 28), 20, 10),
+            MyBlock((10, 28, 28), 10, 10),
+            MyBlock((10, 28, 28), 10, 10, normalize=False)
+        )
+
+        self.conv_out = nn.Conv2d(10, 1 , 3, 1, 1)
+
+    def forward(self, x, t):
+
+        # x is (N, 2, 28, 28) (image with positional embedding stacked on channel dimension)
+        t = self.time_embed(t)
+        n = len(x)
+
+        out1 = self.b1(x + self.te1(t).reshape(n, -1, 1, 1)) # (N, 10, 28, 28)
+        out2 = self.b2(self.down1(out1) + self.te2(t).reshape(n, -1, 1, 1)) # (N, 20, 14, 14)
+        out3 = self.b3(self.down2(out2) + self.te3(t).reshape(n, -1, 1, 1)) # (N, 40, 7, 7)
+
+        out_mid = self.b_mid(self.down3(out3) + self.te_mid(t).reshape(n, -1, 1, 1)) # (N, 40, 3, 3)
+
+        out4 = torch.cat((out3, self.up1(out_mid)), dim=1) # (N, 80, 7, 7)
+        out4 = self.b4(out4 + self.te4(t).reshape(n, -1, 1, 1)) # (N, 20, 14, 14)
+
+        out5 = torch.cat((out2, self.up2(out4)), dim=1) # (N, 40, 14, 14)
+        out5 = self.b5(out5 + self.te5(t).reshape(n, -1, 1, 1)) # (N, 10, 14, 14)
+
+        out = torch.cat((out1, self.up3(out5)), dim=1) # (N, 20, 28, 28)
+        out = self.b_out(out + self.te_out(t).reshape(n, -1, 1, 1)) # (N, 1, 28, 28)
+
+        out = self.conv_out(out)
+
+        return out
+
+
+    def _make_te(self, dim_in, dim_out):
+        return nn.Sequential(
+        nn.Linear(dim_in, dim_out),
+        nn.SiLU(),
+        nn.Linear(dim_out, dim_out)
+    )
 
 def show_images(images, title=""):
 
@@ -36,6 +182,82 @@ def show_images(images, title=""):
 
     # Display the figure
     plt.show()
+
+def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif_name="sampling.gif", c=1, h=28, w=28):
+
+    # Given a DDPM model, a number of samples to be generated and a device, return some newly generated samples.
+    frame_idxs = np.linspace(0, ddpm.n_steps, frames_per_gif).astype(np.uint)
+    frames = []
+
+    with torch.no_grad():
+        if device is None:
+            device = ddpm.device
+
+        # Starting from random noise
+        x = torch.randn(n_samples, c, h, w).to(device)
+
+        for idx, t in enumerate(list(range(ddpm.n_steps))[::-1]):
+
+            # Estimating noise to be removed
+            time_tensor = (torch.ones(n_samples, 1) * t).to(device).long()
+            eta_theta = ddpm.backward(x, time_tensor)
+
+            alpha_t = ddpm.alphas[t]
+            alpha_t_bar = ddpm.alpha_bars[t]
+
+            # Partially denoising the image
+            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_t_bar).sqrt() * eta_theta)
+
+            if t > 0:
+                z = torch.randn(n_samples, c, h, w).to(device)
+
+                # Option 1: sigma_t squared = beta_t
+                beta_t = ddpm.betas[t]
+                sigma_t = beta_t.sqrt()
+
+                # Option 2: sigma_t squared = beta_tilde_t
+
+                # Adding some more noise like in Langevin Dynamics fashion
+                x = x + sigma_t * z
+            
+            # Adding frames to the GIF
+            if idx in frame_idxs or t == 0:
+
+                # Putting digits in range [0, 255]
+                normalized = x.clone()
+                for i in range(len(normalized)):
+                    normalized[i] -= torch.min(normalized[i])
+                    normalized[i] *= 255 / torch.max(normalized[i])
+
+                # Reshaping batch (n, c, h, w) to be a (as much as it gets) square frame
+                frame = einops.rearrange(normalized, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=int(n_samples ** 0.5))
+                frame = frame.cpu().numpy().astype(np.uint8)
+
+                # Rendering frame
+                frames.append(frame)
+
+    # Storing the GIF
+    with imageio.get_writer(gif_name, mode="I") as writer:
+        for idx, frame in enumerate(frames):
+            writer.append_data(frame)
+            if idx == len(frames) - 1:
+                for _ in range(frames_per_gif // 3):
+                    writer.append_data(frames[-1])
+
+    return x
+
+def show_forward(ddpm, loader, device):
+
+    # Showing the forward process
+    for batch in loader:
+        imgs = batch[0]
+
+        show_images(imgs, "Original Images")
+
+        for percent in [0.25, 0.5, 0.75, 1]:
+            show_images(ddpm(imgs.to(device), [int(percent * ddpm.n_steps) - 1 for _ in range(len(imgs))]), f"DDPM Noisy images {int(percent * 100)}%")
+
+        break
 
 class DDPM(nn.Module):
     def __init__(self, network, n_steps=200, min_beta=10**-4, max_beta=0.02, device=torch.device("cpu"), image_ch=(1, 28, 28)):
@@ -67,7 +289,53 @@ class DDPM(nn.Module):
         # Run each image through the network for each timestep t in the vector t.
         # The network returns its estimation of the noise that was added.
         return self.network(x, t)
+    
+def training_loop(ddpm, loader, n_epochs, optim, device, display=False, store_path="ddpm_model.pt"):
+    mse = nn.MSELoss()
+    best_loss = float("inf")
+    n_steps = ddpm.n_steps
 
+    for epoch in tqdm(range(n_epochs), desc=f"Training progress", colour="#00ff00"):
+
+        epoch_loss = 0.0
+        
+        for step, batch in enumerate(tqdm(loader, leave=False, desc=f"Epoch {epoch + 1}/{n_epochs}", colour="#005500")):
+
+            # Loading data
+            x0 = batch[0].to(device)
+            n = len(x0)
+
+            # Picking some noise for each of the images in the batch, a timestep and the respective alpha_bars
+            eta = torch.randn_like(x0).to(device)
+            t = torch.randint(0, n_steps, (n, )).to(device)
+
+            # Computing the noisy image based on x0 and the timestep (forward process)
+            noisy_imgs = ddpm(x0, t, eta)
+
+            # Getting model estimation of noise based on the images and the timestep
+            eta_theta = ddpm.backward(noisy_imgs, t.reshape(n, -1))
+
+            # Optimizing the MSE between the noise plugged and the predicted noise
+            loss = mse(eta_theta, eta)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            epoch_loss += loss.item() * len(x0) / len(loader.dataset)
+
+        # Display images generated at this epoch
+        if display:
+            show_images(generate_new_images(ddpm, device=device), f"Images generated at epoch {epoch + 1}")
+
+        log_string = f"Loss at epoch {epoch + 1}: {epoch_loss:.3f}"
+
+        # Storing the model
+        if best_loss > epoch_loss:
+            best_loss = epoch_loss
+            torch.save(ddpm.state_dict(), store_path)
+            log_string += " --> Best model ever (stored)"
+
+        print(log_string)
 
 SEED = 0
 random.seed(SEED)
@@ -79,7 +347,7 @@ FASHION_PATH = f"ddpm_model_fashion.pt"
 
 # Hyperparameters
 batch_size = 128
-n_epochs = 20
+n_epochs = 1
 lr = 0.001
 
 # Transforms for images in dataset (convert to tensor and normalize)
@@ -88,23 +356,37 @@ transform = Compose([
     Lambda(lambda x: (x - 0.5) * 2)
 ])
 
-# Download dataset
+# Download dataset if necessary
 mnist_data = MNIST("./data", download=True, train=True, transform=transform)
 
 # Prepare data loader
-data = DataLoader(mnist_data, batch_size, shuffle=True)
+data_loader = DataLoader(mnist_data, batch_size, shuffle=True)
 
-show_images(next(iter(data))[0])
+# Optionally, show preview of images in the dataset
+#show_images(next(iter(data_loader))[0])
 
+# Defining the model
+n_steps = 1000
+min_beta = 10 ** -4
+max_beta = 0.02
+device = torch.device("cpu")
 
-'''
-test = next(iter(data))
+ddpm = DDPM(UNet(n_steps))
 
-print(type(test))
-print(len(test))
-print(type(test[0]))
+# Optionally, show the diffusion (forward) process
+#show_forward(ddpm, data_loader, device)
 
-transform = T.ToPILImage()
-img = transform(test[0][0])
-img.show()
-'''
+# Optionally, show the denoising (backward) process
+#generated = generate_new_images(ddpm, gif_name="before_training.gif")
+#show_images(generated, "Images generated before training")
+
+# Loading the pre-trained model
+store_path = "ddpm_model.pt"
+ddpm.load_state_dict(torch.load(store_path, map_location=device))
+training_loop(ddpm=ddpm, 
+              loader=data_loader, 
+              n_epochs=n_epochs, 
+              optim=Adam(ddpm.parameters(), lr), 
+              device=device, 
+              display=True, 
+              store_path=store_path)
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # DeepLearningExamples #
 
+## References ##
+
 <ul>
     <li>Denoising Diffusion Probabilistic Model (DDPM) - https://medium.com/mlearning-ai/enerating-images-with-ddpms-a-pytorch-implementation-cef5a2ba8cb1</li>
+    <li> Neural Radiance Field (NeRF) - https://towardsdatascience.com/its-nerf-from-nothing-build-a-vanilla-nerf-with-pytorch-7846e4c45666</li>
+</ul>
+
+## Other Links ##
+<ul>
+    <li>https://github.com/ashawkey/stable-dreamfusion</li>
+    <li>https://dreamfusion3d.github.io/</li>
+    <li>https://github.com/lucidrains/imagen-pytorch</li>
 </ul>
