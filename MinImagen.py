ccb2ff2e0e9cf17281d58880d400ef700a878b45
ttdow <travis.t.dow@gmail.com>
2023-04-25 16:22:33 -0600

    Updates to MinImagen implementation.

--- MinImagen.py
+++ MinImagen.py
@@ -2,6 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+def exists(val):
+    return val is not None
+
+# Returns the input value 'val' unless it is 'None', in which case the default 'd' is returned if it is a value or
+# d() if it is a callable.
+def default(val, d):
+    
+    if exists(val):
+        return val
+    return d() if callable(d) else d
+
+# Extracts values from a using t as indices
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1, ) * (len(x_shape) - 1)))
+
 class GaussianDiffusion(nn.Module):
 
     def __init__(self, *, timesteps):
@@ -47,6 +64,22 @@ class GaussianDiffusion(nn.Module):
         # Buffers for calculating the q_posterior mean.
         register_buffer('posterior_mean_coef1', betas * torch.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
         register_buffer('posterior_mean_coef2', (1. - alphas_cumprod_prev) * torch.sqrt(alphas) / (1. - alphas_cumprod))
+
+    # Sample from q at given timestep
+    def q_sample(self, x_start, t, noise):
+
+        noise = default(noise, lambda: torch.randn_like(x_start))
+
+        noised = (extract(self.sqrt_alphas_cumprod, t, x_start.shape) * x_start + extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape) * noise)
+
+        return noised
+    
+    # Given a noised image and its noise component, calculate the unnoised image
+    def predict_start_from_noise(self, x_t, t, noise):
+        return (extract(self.sqrt_recip_alphas_cumprod, t, x_t.shape) * x_t - extract(self.sqrt_recipm1_alphas_cumprod, t, x_t.shape) * noise)
+    
+    def q_posterior(self, x_start, x_t, t):
+        
 
 print(f'PyTorch version: {torch.__version__}')
 print('*'*10)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <ul>
     <li>Denoising Diffusion Probabilistic Model (DDPM) - https://medium.com/mlearning-ai/enerating-images-with-ddpms-a-pytorch-implementation-cef5a2ba8cb1</li>
     <li> Neural Radiance Field (NeRF) - https://towardsdatascience.com/its-nerf-from-nothing-build-a-vanilla-nerf-with-pytorch-7846e4c45666</li>
-    <li> MinImagen - https://www.assemblyai.com/blog/minimagen-build-your-own-imagen-text-to-image-model/</li>
+    <li> MinImagen - https://www.assemblyai.com/blog/minimagen-build-your-own-imagen-text-to-image-model/ + https://github.com/AssemblyAI-Examples/MinImagen</li>
 </ul>
 
 ## Other Links ##
